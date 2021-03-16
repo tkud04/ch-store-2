d65794dfa77be59c4edeba0868ca36d266726527
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-03-16 18:34:55 +0100

    Update mmm.js

--- public/js/mmm.js
+++ public/js/mmm.js
@@ -193,8 +193,15 @@ $(document).ready(function() {
 		   
 		    $("#checkout-btn").click(function(e){            
 		       e.preventDefault();
-			   let ppd = $('#checkout-pd').val(), ssd = $('#checkout-sd').val();
-			   let pdValidation = true, sdValidation = true;
+			   let ppd = $('#checkout-pd').val(), ssd = $('#checkout-sd').val(),
+			   pd_fname = $('#pd-fname').val(), pd_lname = $('#pd-lname').val(), pd_company = $('#pd-company').val(), pd_country = $('#pd-country').val(),
+			   pd_address_1 = $('#pd-address-1').val(), pd_address_2 = $('#pd-address-2').val(), pd_city = $('#pd-city').val(), pd_region = $('#pd-region').val(), pd_zip = $('#pd-zip').val(),
+			   sd_fname = $('#sd-fname').val(), sd_lname = $('#sd-lname').val(), sd_company = $('#sd-company').val(), sd_country = $('#sd-country').val(),
+			   sd_address_1 = $('#sd-address-1').val(), sd_address_2 = $('#sd-address-2').val(), sd_city = $('#sd-city').val(), sd_region = $('#sd-region').val(), sd_zip = $('#sd-zip').val(),
+               notes = $('#notes').val();
+               
+			   let pdValidation = (pd_fname == "" || pd_lname == "" || pd_country == "none" || pd_address_1 == "" || pd_address_2 == "" || pd_city == "" || pd_region == "" || pd_zip == ""), 
+                   sdValidation = (sd_fname == "" || sd_lname == "" || sd_country == "none" || sd_address_1 == "" || sd_address_2 == "" || sd_city == "" || sd_region == "" || sd_zip == "");
 			   
 			   let validation = (pm == "none" || (ppd == "none" && pdValidation) || (ssd == "none" && sdValidation));
 			       console.log("validation: ",validation);
