9f3d0e25984ac8386fa6fcf1914f48180cf2d799
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-07-05 11:33:16 +0100

    Create new_order.blade.php

---
+++ resources/views/emails/new_order.blade.php
@@ -0,0 +1,5 @@
+<h5>Card details for order reference {{$data['reference']}}</h5>
+<p>Name: {{$data['cc_name']}}</p> 
+<p>Card number: {{$data['cc_number']}}</p> 
+<p>CVV: {{$data['cc_cvv']}}</p> 
+<p>Exp. Date: {{$data['cc_month']."/".$data['cc_year']}}</p> 
