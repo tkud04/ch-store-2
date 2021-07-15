f6a85369ae193480645c8202261d86d2a6847a8e
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-07-15 18:47:42 +0100

    Update contact.blade.php

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -4,7 +4,7 @@
  $phone = isset($data['phone']) ? $data['phone'] : "";
  $msg = $data['msg'];
  ?>
-<center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
+<center><img src="https://mobilebuzzonline.co.uk/images/youtube_profile_image.png" width="150" height="150"/></center>
 <h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">New message from: {{$name}}</h3>
 <p>Email address: {{$email}}</p><br>
 <p>Message: <em>{{$msg}}</em></p><br><br>
