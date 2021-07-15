ee2a5b7fc7209d9b8b70677fbc0ce28c46673c53
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-07-15 18:18:13 +0100

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,14 @@
 $title = "Contact Us";
 $ph = true;
 $pcClass = "";
+
+$n = ""; $e = "";
+
+if($user != null)
+{
+  $n = $user->fname." ".$user->lname;
+  $e = $user->email; 
+}
 ?>
 @extends('layout')
 
@@ -14,14 +22,7 @@ $pcClass = "";
                                 <p>Looking for help? Fill the form and start a new adventure.</p>
 
                                 <h4 class="mb-1 text-uppercase">Address</h4>
-                                <p>UK Flag 54 Mortlake Road, Ilford, IG1 2SX</p>
-                                
-								<h4 class="mb-1 text-uppercase">Working days / hours</h4>
-                                <p>
-								  <a href="javascript:void(0)">Mon - Fri / 8:30 AM - 7:00 PM</a><br>
-										<a href="javascript:void(0)">Sat / 9:00 AM - 6:00 PM</a><br>
-										<a href="javascript:void(0)">Sun / Closed</a>
-								</p>
+                                <p>UK Flag 54 Mortlake Road, Ilford, IG1 2SX</p>                             							
 								
                                 <h4 class="mb-1 text-uppercase">Phone</h4>
                                 <p><a href="javascript:void(0)">+44 7451 233887</a></p>
@@ -30,24 +31,32 @@ $pcClass = "";
                                 <p>
                                     <a href="mailto:sales@mobilebuzzonline.co.uk">sales@mobilebuzzonline.co.uk</a><br>
                                 </p>
+				
+				<h4 class="mb-1 text-uppercase">Working days / hours</h4>
+                                <p>
+				  <a href="javascript:void(0)">Mon - Fri / 8:30 AM - 7:00 PM</a><br>
+				  <a href="javascript:void(0)">Sat / 9:00 AM - 6:00 PM</a><br>
+				  <a href="javascript:void(0)">Sun / Closed</a>
+				</p>
                             </div>
                             <div class="col-lg-9 col-md-8 col-xs-7">
-                                <form class="ml-lg-2 pt-8 pb-10 pl-4 pr-4 pl-lg-6 pr-lg-6 grey-section" action="{{url('contact')}}">
+                                <form class="ml-lg-2 pt-8 pb-10 pl-4 pr-4 pl-lg-6 pr-lg-6 grey-section" action="{{url('contact')}}" method="post">
+				 {!! csrf_field() !!}
                                     <h3 class="ls-m mb-1">Let’s Connect</h3>
                                     <p class="text-grey">Your email address will not be published. Required fields are
                                         marked *</p>
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
-                                            <input class="form-control" type="text" name="name" placeholder="Name *" required="">
+                                            <input class="form-control" type="text" name="name" placeholder="Name *" value="{{$n}}" required="">
                                         </div>
                                         <div class="col-md-6 mb-4">
-                                            <input class="form-control" type="email" name="email" placeholder="Email *" required="">
+                                            <input class="form-control" type="email" name="email" placeholder="Email *" value="{{$e}}" required="">
                                         </div>
                                         <div class="col-12 mb-4">
                                             <textarea class="form-control" name="msg" required="" placeholder="Your Message *"></textarea>
                                         </div>
                                     </div>
-                                    <button class="btn btn-md btn-primary mb-2">Send</button>
+                                    <button class="btn btn-md btn-primary mb-2" type="submit">Send</button>
                                 </form>
                             </div>
                         </div>
