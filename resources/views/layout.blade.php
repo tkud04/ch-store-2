c7b9b586e7e01086d7dd4293c1acd5bcb810e5e5
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-07-25 15:20:14 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -102,7 +102,7 @@
 		 <div class="header-top">
                 <div class="container">
                     <div class="header-left">
-                        <p class="welcome-msg">Welcome to Mobile Buzz! | <a href="{{$xu}}">{{$xt}}</a></p>
+                        <p class="welcome-msg">Welcome to Mobile Buzz! | <a href="{{$xu}}"><strong class="text-primary">{{$xt}}</strong></a></p>
                     </div>
                     <div class="header-right">
 					 <a href="tel:{{$tel}}" class="calll"><span class="text-uppercase font-weight-semi-bold ls-l"></span><strong class="text-primary">{{$call}}</strong></a>
